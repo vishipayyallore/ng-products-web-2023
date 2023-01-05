e7537e890dc68bbdfe72cca8bbcd36b3127899a1
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-01-05 08:46:00 +0530

    Update footer.component.ts

--- src/app/modules/shared/layout/footer/footer.component.ts
+++ src/app/modules/shared/layout/footer/footer.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { VersionService } from '~/app/services/version.service';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent {
-  version = '1.0.1';
+export class FooterComponent implements OnInit {
+
+  version?: string = '1.0.0';
+
+  constructor(private versionService: VersionService) { }
+
+  ngOnInit(): void {
+    this.version = this.versionService.getVersion();
+  }
+
+
+
 }
