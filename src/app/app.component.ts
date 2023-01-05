9c4da1145c08256949b416e4f9f241282442a4ab
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-01-05 09:32:52 +0530

    Added Paginator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MessageBandService } from './services/message-band.service';
 
 @Component({
@@ -6,11 +6,17 @@ import { MessageBandService } from './services/message-band.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   
   title = 'Products Store';
+  
   message?: string = 'Messages will be displayed. Example: Offers etc.';
+  showMessageBand: boolean = false;
 
   constructor(public messageBandService: MessageBandService) { }
+
+  ngOnInit(): void {
+    this.showMessageBand = this.messageBandService.getShowMessageBand();
+  }
   
 }
