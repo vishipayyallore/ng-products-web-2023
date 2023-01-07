4bb1257fd492ec5ad1da92c4bb1e01d653ea5ce0
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-01-07 19:42:48 +0530

    Progress Spinner

---
+++ src/app/core/components/progress-spinner/progress-spinner.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProgressSpinnerComponent } from './progress-spinner.component';
+
+
+
+@NgModule({
+  declarations: [
+    ProgressSpinnerComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ProgressSpinnerModule { }
