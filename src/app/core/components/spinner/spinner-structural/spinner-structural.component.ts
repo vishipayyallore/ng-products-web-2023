1c251b7e093f1bf95e0e39549844f0977df4a7c5
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-01-21 15:38:24 +0530

    Initial Version

---
+++ src/app/core/components/spinner/spinner-structural/spinner-structural.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-spinner-structural',
+  templateUrl: './spinner-structural.component.html',
+  styleUrls: ['./spinner-structural.component.scss']
+})
+export class SpinnerStructuralComponent {
+
+}
---
+++ src/app/core/components/spinner/spinner.directive.ts
@@ -0,0 +1,25 @@
+import { Directive, Input, TemplateRef, ViewContainerRef } from '@angular/core';
+
+import { SpinnerStructuralComponent } from './spinner-structural/spinner-structural.component';
+
+@Directive({
+  standalone: true,
+  selector: '[appSpinner]',
+})
+export class SpinnerDirective {
+  @Input()
+  set appSpinner(isLoading: boolean) {
+    if (isLoading) {
+      this.view.clear();
+      this.view.createComponent(SpinnerStructuralComponent);
+    } else {
+      this.view.clear();
+      this.view.createEmbeddedView(this.template);
+    }
+  }
+
+  constructor(
+    private view: ViewContainerRef,
+    readonly template: TemplateRef<AnimationPlayState>
+  ) {}
+}
